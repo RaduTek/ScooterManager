8204429b5e513257dca9d7ee4754c23af5b67160
RaduTek <radutek13@gmail.com>
2023-10-28 10:50:09 +0300

    Add test map

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,29 @@
 import "./App.css";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 
 function App() {
-    return <div className="App">Hello, world!</div>;
+    return (
+        <>
+            <div className="App">Hello, world!</div>
+            <MapContainer
+                center={[51.505, -0.09]}
+                zoom={13}
+                scrollWheelZoom={false}
+            >
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <Marker position={[51.505, -0.09]}>
+                    <Popup>
+                        A pretty CSS3 popup. <br /> Easily customizable.
+                        <button>A</button>
+                    </Popup>
+                </Marker>
+            </MapContainer>
+        </>
+    );
 }
 
 export default App;
