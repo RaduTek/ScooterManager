c5c0062f64818ed0227cde7d4b1a189438dc0a43
RaduTek <radutek13@gmail.com>
2023-10-29 00:22:25 +0300

    Scooter component

---
+++ frontend/src/components/Scooter/Scooter.jsx
@@ -0,0 +1,98 @@
+import Icon from "@mdi/react";
+import { Marker, Popup, useMap } from "react-leaflet";
+import Button from "../Controls/Button";
+import {
+    mdiBatteryHigh,
+    mdiBatteryLow,
+    mdiBatteryMedium,
+    mdiBatteryRemoveOutline,
+    mdiMapMarkerDistance,
+} from "@mdi/js";
+import {
+    iconScooterGreen,
+    iconScooterRed,
+    iconScooterYellow,
+} from "../../mapIcons";
+import { useNavigate } from "react-router-dom";
+
+function Scooter({
+    id = "0",
+    location = [0, 0],
+    available = false,
+    batteryLevel = 50,
+    levelRangeRatio = 0.3,
+}) {
+    const navigate = useNavigate();
+    const map = useMap();
+
+    return (
+        <Marker
+            position={location}
+            icon={
+                available
+                    ? batteryLevel > 20
+                        ? iconScooterGreen
+                        : iconScooterYellow
+                    : iconScooterRed
+            }
+        >
+            <Popup>
+                <div className="scooter-popup">
+                    <div>
+                        <h1>Scooter</h1>
+                        <h2>
+                            {available
+                                ? "Available" +
+                                  (batteryLevel < 20 ? ", low on charge" : "")
+                                : "In Use"}
+                        </h2>
+                    </div>
+                    <div className="stats">
+                        <Icon
+                            path={
+                                batteryLevel > 80
+                                    ? mdiBatteryHigh
+                                    : batteryLevel > 40
+                                    ? mdiBatteryMedium
+                                    : batteryLevel > 20
+                                    ? mdiBatteryLow
+                                    : mdiBatteryRemoveOutline
+                            }
+                            color={
+                                batteryLevel > 80
+                                    ? "green"
+                                    : batteryLevel > 60
+                                    ? "darkkhaki"
+                                    : batteryLevel > 40
+                                    ? "orange"
+                                    : "red"
+                            }
+                        />
+                        <span>{batteryLevel}%</span>
+                        <Icon path={mdiMapMarkerDistance} />
+                        <span>
+                            {Math.round(batteryLevel * levelRangeRatio * 10) /
+                                10}{" "}
+                            km
+                        </span>
+                    </div>
+                    {available && (
+                        <Button
+                            color="blue"
+                            onClick={() => {
+                                map.flyTo(location, 18);
+                                map.closePopup();
+                                navigate("rent");
+                            }}
+                        >
+                            Rent
+                        </Button>
+                    )}
+                    <Button>More Information</Button>
+                </div>
+            </Popup>
+        </Marker>
+    );
+}
+
+export default Scooter;
