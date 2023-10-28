c88ac2b94c233ea34cf48fdbe18d8a130d3e6c36
RaduTek <radutek13@gmail.com>
2023-10-29 00:23:49 +0300

    New routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,26 @@
 import "./App.css";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import "leaflet/dist/leaflet.css";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Map from "./pages/Map/Map";
+import Rent from "./pages/Rent";
+import Search from "./pages/Search";
+import Account from "./pages/Account";
+import SignUp from "./pages/SignUp";
 
 function App() {
     return (
-        <>
-            <div className="App">Hello, world!</div>
-            <MapContainer
-                center={[51.505, -0.09]}
-                zoom={13}
-                scrollWheelZoom={false}
-            >
-                <TileLayer
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
-                <Marker position={[51.505, -0.09]}>
-                    <Popup>
-                        A pretty CSS3 popup. <br /> Easily customizable.
-                        <button>A</button>
-                    </Popup>
-                </Marker>
-            </MapContainer>
-        </>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/">
+                    <Route index element={<Navigate to="map" />} />
+                    <Route path="map" element={<Map />}>
+                        <Route path="rent" element={<Rent />} />
+                        <Route path="search" element={<Search />} />
+                        <Route path="account" element={<Account />} />
+                        <Route path="signup" element={<SignUp />} />
+                    </Route>
+                </Route>
+            </Routes>
+        </BrowserRouter>
     );
 }
 
