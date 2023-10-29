f09976c951dce39f83d7ec6466a7697ffb9a12bf
RaduTek <radutek13@gmail.com>
2023-10-29 23:20:12 +0200

    Remove old search page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,6 @@ import "./App.css";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Map from "./pages/Map/Map";
 import Rent from "./pages/Rent";
-import Search from "./pages/Search";
 import Account from "./pages/Account";
 import SignUp from "./pages/SignUp";
 
@@ -14,7 +13,6 @@ function App() {
                     <Route index element={<Navigate to="map" />} />
                     <Route path="map" element={<Map />}>
                         <Route path="rent" element={<Rent />} />
-                        <Route path="search" element={<Search />} />
                         <Route path="account" element={<Account />} />
                         <Route path="signup" element={<SignUp />} />
                     </Route>
